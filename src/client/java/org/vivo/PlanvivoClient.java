57d44c70b233cdc06b3e86b5784b0fc13f6c9ef5
savvacorgi <rtynoot@gmail.com>
2024-07-08 14:57:42 +0300

    обнова когда рак на горе свиснет

---
+++ src/client/java/org/vivo/PlanvivoClient.java
@@ -0,0 +1,10 @@
+package org.vivo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PlanvivoClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		
+	}
+}
---
+++ src/main/java/org/vivo/Planvivo.java
@@ -0,0 +1,19 @@
+package org.vivo;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Planvivo implements ModInitializer {
+
+	public static final public String MODID = "planvivo"
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("MODID");
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Planvivo mod activatet!");
+	}
+}
