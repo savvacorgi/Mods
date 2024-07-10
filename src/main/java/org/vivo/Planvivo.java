f18dc43dc9b12f571400771d18dcfe3b2555e118
savvacorgi <rtynoot@gmail.com>
2024-07-10 13:10:24 +0300

    add test item

--- src/main/java/org/vivo/Planvivo.java
+++ src/main/java/org/vivo/Planvivo.java
@@ -4,15 +4,15 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.vivo.item.ModItems;
 
 public class Planvivo implements ModInitializer {
 
-	public static final String MODID = "planvivo";
-    public static final Logger LOGGER = LoggerFactory.getLogger("MODID");
+	public static final String MOD_ID = "planvivo";
+    public static final Logger LOGGER = LoggerFactory.getLogger("MOD_ID");
 
 	@Override
 	public void onInitialize() {
-
-		LOGGER.info("Planvivo mod activatet!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/org/vivo/item/ModItems.java
@@ -0,0 +1,32 @@
+package org.vivo.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import org.vivo.Planvivo;
+
+public class ModItems {
+
+    public static final Item VIVO = registerItem("vivo", new Item(new FabricItemSettings()));
+
+    private static void addItemToGroup(FabricItemGroupEntries entries){
+        entries.add(VIVO);
+    }
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(Planvivo.MOD_ID, name), item);
+    }
+
+
+    public static void registerModItems() {
+        Planvivo.LOGGER.info("register...");
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemToGroup);
+    }
+}
